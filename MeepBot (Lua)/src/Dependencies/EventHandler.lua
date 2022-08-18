4cb7ad2a5830fab8575f54e8555a8175bab23b98
KlonHowWhy <96662615+KlonHowWhy@users.noreply.github.com>
2022-08-19 08:18:32 +0900

    Delete unnecessary comments

--- MeepBot (Lua)/src/Dependencies/EventHandler.lua
+++ MeepBot (Lua)/src/Dependencies/EventHandler.lua
@@ -13,26 +13,3 @@ function Events.OnMessageSent(Runtime, Message)
 end
 
 return Events
-
---[[
-    -- Regular commands without parameters
-    if Content == "hello" then
-        Message:reply(string.format("Konnichiwa, %s-sama!", Message.author.mentionString))
-    end
-
-    -- Iterations of the commands (commands with parameters)
-    local Command, param = Content:match"(.-) (.+)"
-    if Command == "hello" then
-        if param == "jp" then
-            Message:reply(string.format(
-                "こんにちは、 %sー様!"
-                , Message.author.mentionString))
-        elseif param == "en" then
-            Message:reply(string.format(
-                "Hello my master, %s-sama!"
-                , Message.author.mentionString))
-        else
-            Message:reply(string.format(
-                "I'm sorry, I don't know that language...\nBut hello my master, %s-sama!",
-                Message.author.mentionString))
-        end]]
